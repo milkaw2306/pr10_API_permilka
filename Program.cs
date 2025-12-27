059c6d23f036cc50d23193af2c5f50974a338d2f
YarikTunev <yariktunev2005@mail.ru>
2025-12-27 10:45:33 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr10_API_permilka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
