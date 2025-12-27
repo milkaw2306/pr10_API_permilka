f7d24a654364111e3b6a6573d964e5bf6398d8f0
YarikTunev <yariktunev2005@mail.ru>
2025-12-27 11:18:06 +0500

    Разработанный метод для получения ответов от GigaChat.permilks

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using pr10_API_permilka.Models.Response;
 
 namespace pr10_API_permilka
 {
@@ -10,6 +12,11 @@ namespace pr10_API_permilka
     {
         static string ClientId = "***";
         static string AuthorizationKey = "***";
+        static async Task Main(string[] args)
+        {
+            string Token = await GetToken(ClientId, AuthorizationKey);
+
+        }
         public static async Task<string> GetToken(string rqUID, string bearer)
         {
             string ReturnToken = null;
@@ -32,17 +39,51 @@ namespace pr10_API_permilka
                     if (Response.IsSuccessStatusCode)
                     {
                         string ResponseContent = await Response.Content.ReadAsStringAsync();
-                        ResponseToken Token = JsonConvert.Deserialize0bject<ResponseToken>(ResponseContent);
+                        ResponseToken Token = JsonConvert.DeserializeObject<ResponseToken>(ResponseContent);
                         ReturnToken = Token.access_token;
                     }
                 }
             }
             return ReturnToken;
         }
-
-        static void Main(string[] args)
+        public static async Task<ResponseMessage> GetAnswer(string token, string message)
         {
+            ResponseMessage responseMessage = null;
+            string Url = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions";
+            using (HttpClientHandler Handler = new HttpClientHandler())
+            {
+                Handler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) => true;
+                using (HttpClient Client = new HttpClient(Handler))
+                {
+                    HttpRequestMessage Request = new HttpRequestMessage(HttpMethod.Post, Url);
+                    Request.Headers.Add("Accept", "application/json");
+                    Request.Headers.Add("Authorization", $"Bearer {token}");
+                    Request.DataRequest = new Request()
+                    {
+                        model = "GigaChat",
+                        stream = false,
+                        repetition_penalty = 1,
+                        messages = new List<Request.Message>()
+                        {
+                            new Request.Message()
+                            {
+                                role = "user",
+                                content = message
+                            }
+                        }
+                    };
 
+                    string JsonContent = JsonConvert.SerializeObject(DataRequest);
+                    Request.Content = new StringContent(JsonContent, Encoding.UTF8, "application/json");
+                    HttpResponseMessage Response = await Client.SendAsync(Request);
+                    if (Response.IsSuccessStatusCode)
+                    {
+                        string ResponseContent = await Response.Content.ReadAsStringAsync();
+                        responseMessage = JsonConvert.DeserializeObject<ResponseMessage>(ResponseContent);
+                    }
+                }
+            }
+            return responseMessage;
         }
     }
 }
